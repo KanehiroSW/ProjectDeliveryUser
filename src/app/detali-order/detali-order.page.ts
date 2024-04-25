08124d9b3efb4efb124ce6bd7f9891bde22dfec2
PieroLeon <pierolh2000@hotmail.com>
2024-04-25 17:03:05 -0500

    DeliveryUserCommit

--- src/app/detali-order/detali-order.page.ts
+++ src/app/detali-order/detali-order.page.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Product {
+  name: string;
+  price: number;
+  image: string;
+}
 @Component({
   selector: 'app-detali-order',
   templateUrl: './detali-order.page.html',
   styleUrls: ['./detali-order.page.scss'],
 })
-export class DetaliOrderPage implements OnInit {
+export class DetaliOrderPage{
 
-  constructor() { }
+  products: Product[] = [
+    {
+      name: 'Galleta Casino de Chocolate',
+      price: 1.50,
+      image: 'assets/imagenes/casinocho.jpg'
+    },
+    {
+      name: 'Chocolate Sublime',
+      price: 2.00,
+      image: 'assets/imagenes/sublime30g.jpg'
+    },
+    {
+      name: 'Gaseosa 500ml. Coca-cola',
+      price: 1.80,
+      image: 'assets/imagenes/coca.jpg'
+    }
+  ];
 
-  ngOnInit() {
+  constructor() {}
+
+  getTotalPrice(): number {
+    return this.products.reduce((acc, product) => acc + product.price, 0);
   }
 
+  confirmPurchase() {
+    // Lógica para confirmar la compra
+    console.log('Compra confirmada');
+  }
 }
--- src/app/store/store.page.ts
+++ src/app/store/store.page.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Product {
+  name: string;
+  price: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.page.html',
   styleUrls: ['./store.page.scss'],
 })
-export class StorePage implements OnInit {
+export class StorePage {
+  products: Product[] = [
+    {
+      name: 'Galleta Casino de Chocolate',
+      price: '1.50',
+      image: 'assets/imagenes/casinocho.jpg'
+    },
+    {
+      name: 'Chocolate Sublime',
+      price: '2.00',
+      image: 'assets/imagenes/sublime30g.jpg'
+    },
+    {
+      name: 'Gaseosa 500ml. Coca-cola',
+      price: '1.80',
+      image: 'assets/imagenes/coca.jpg'
+    }
+  ];
 
-  constructor() { }
+  filteredProducts: Product[];
 
-  ngOnInit() {
+  constructor() {
+    // Inicializar la lista de productos filtrados con todos los productos al principio
+    this.filteredProducts = this.products;
   }
 
+  // Método para manejar el cambio en el componente de búsqueda
+  onSearch(event: CustomEvent) {
+    const searchTerm = event.detail.value;
+    if (searchTerm.trim() !== '') {
+      // Filtrar productos basados en el término de búsqueda
+      this.filteredProducts = this.products.filter(product =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      // Si el campo de búsqueda está vacío, mostrar todos los productos
+      this.filteredProducts = this.products;
+    }
+  }
+
+  addToCart(product: Product) {
+    // Lógica para agregar producto al carrito
+    console.log('Producto añadido al carrito:', product);
+  }
 }
+
+
+
+
