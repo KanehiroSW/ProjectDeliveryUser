e1251b0a82781758af31d4afa24ffafa8e745152
KanehiroSW <kanehirosw@gmail.com>
2024-04-27 19:05:39 -0500

    Test Camara

--- src/app/detali-shipping/detali-shipping.page.ts
+++ src/app/detali-shipping/detali-shipping.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 @Component({
   selector: 'app-detali-shipping',
   templateUrl: './detali-shipping.page.html',
   styleUrls: ['./detali-shipping.page.scss'],
 })
 export class DetaliShippingPage implements OnInit {
+  
+  fotoTomada: string | undefined;
 
   constructor() { }
 
   ngOnInit() {
   }
+  async tomarFoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+      source: CameraSource.Camera
+    });
 
+    this.fotoTomada = 'data:image/jpeg;base64,' + image.base64String;
+  }
 }
